c78029364464313b2702382b9a177f77d7999616
johnnysn <johnnysn@gmail.com>
2023-10-17 07:01:06 -0300

    Configuração inicial de projeto

---
+++ src/app/fonts.ts
@@ -0,0 +1,4 @@
+import { Bebas_Neue, Poppins } from 'next/font/google'
+
+export const bebas = Bebas_Neue({ weight: ['400'], subsets: ['latin'] });
+export const poppins = Poppins({ weight: ['200', '300', '400', '500', '700'], subsets: ['latin'] });

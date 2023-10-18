f19f1da33a1ac76c9877217d096eb205ce52f0f4
johnnysn <johnnysn@gmail.com>
2023-10-18 08:48:58 -0300

    Criar outras páginas do app

---
+++ src/app/about/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <div className="px-2 md:px-6">Sobre o intervals</div>
+}
---
+++ src/app/treinos/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return <section id="treinos" className="px-2 md:px-6">
+        Treinos
+    </section>;
+}
